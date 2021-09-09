5dbdbc8abe4314888c4cf94c399c63418be869e8
Tobias van Beek <t.vanbeek@tjvb.nl>
2021-09-09 17:55:07 +0200

    Start with a test for the GitFileSystemHashFinder

---
+++ tests/HashFinders/GitFileSystemHashFinderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TJVB\GitHash\Tests\HashFinders;
+
+use TJVB\GitHash\HashFinders\GitFileSystemHashFinder;
+use TJVB\GitHash\Tests\TestCase;
+use TJVB\GitHash\Values\GitHash;
+
+class GitFileSystemHashFinderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function weSeeThatTheFinderIsAvailable(): void
+    {
+        // setup / mock
+
+        // run
+        $finder = new GitFileSystemHashFinder();
+
+        // verify/assert
+        $this->assertTrue($finder->isAvailable());
+    }
+
+    /**
+     * @test
+     */
+    public function weCanFindAHashFromTheRepositoryRoot(): void
+    {
+        // setup / mock
+
+        // run
+        $finder = new GitFileSystemHashFinder();
+        $result = $finder->findHash(self::PROJECT_ROOT);
+
+        // verify/assert
+        // @TODO work with a fixed git directory to get a fixed hash
+        $this->assertInstanceOf(GitHash::class, $result);
+    }
+}
